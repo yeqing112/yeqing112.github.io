eb364a44178d4d8a5764b3abcef6d35ce7aac800
yeqing112 <51025826@qq.com>
2020-04-27 23:43:13 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -50,3 +50,17 @@ $(document).ready(function() {
     $('.btn-mobile-menu__icon').toggleClass('fa fa-list fa fa-angle-up animated fadeIn');
   });
 });
+
+document.querySelector('.page-search-input').addEventListener('keyup',function(e){
+    var archive = document.getElementsByClassName('archive-item-link');
+    for (var i = 0; i < archive.length; i++){
+        if( archive[i].title.toLowerCase().indexOf(this.value.toLowerCase()) > -1 ) {
+            archive[i].closest('li').style.display = '';
+        } else {
+            archive[i].closest('li').style.display = 'none';
+        }
+    }
+    if(e.keyCode == 13){
+        window.open('https://www.google.com/#q=site:blog.fooleap.org+'+this.value);
+    }
+})
