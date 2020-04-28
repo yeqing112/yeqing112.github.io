125055cd825e2d910ae2defaa365625ae6614938
yeqing112 <51025826@qq.com>
2020-04-28 15:46:22 +0800

    Update search_kw.js

--- js/search_kw.js
+++ js/search_kw.js
@@ -2,6 +2,16 @@ window.addEventListener('beforeunload', function (event) {
   document.getElementById('menu').checked = false;
 });
 
+function getQuery(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == variable) { return pair[1]; }
+  }
+  return (false);
+}
+
 document.addEventListener('DOMContentLoaded', function (event) {
 
   var curYear = new Date().getFullYear();
