50325069756cc5e2a196375d01dd8fbacc3b252b
yeqing112 <51025826@qq.com>
2020-04-28 12:20:22 +0800

    Update search_kw.js

--- js/search_kw.js
+++ js/search_kw.js
@@ -1,3 +1,13 @@
+ function getQuery(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == variable) { return pair[1]; }
+  }
+  return (false);
+}
+ 
     var keyword = getQuery('keyword');
     var searchData;
     var input = document.querySelector('.search-input');
