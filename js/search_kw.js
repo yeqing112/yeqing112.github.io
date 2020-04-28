e94e65349b6813ebe5c8a910e805f4635d43cdff
yeqing112 <51025826@qq.com>
2020-04-28 11:14:54 +0800

    1

--- js/search_kw.js
+++ js/search_kw.js
@@ -1,4 +1,45 @@
-function search(keyword) {
+  if (page.url == '/archive/') {
+    document.querySelector('.page-search-input').addEventListener('keyup', function (e) {
+      var archive = document.getElementsByClassName('archive-item-link');
+      for (var i = 0; i < archive.length; i++) {
+        if (archive[i].title.toLowerCase().indexOf(this.value.toLowerCase()) > -1) {
+          archive[i].closest('li').style.display = 'block';
+        } else {
+          archive[i].closest('li').style.display = 'none';
+        }
+      }
+      if (e.keyCode == 13) {
+        location.href = '/search.html?keyword=' + this.value;
+      }
+    })
+  }
+
+  if (page.url == '/search/') {
+    var keyword = getQuery('keyword');
+    var searchData;
+    var input = document.querySelector('.search-input');
+    var result = document.querySelector('.search-result');
+    var xhrSearch = new XMLHttpRequest();
+    xhrSearch.open('GET', '/search.json', true);
+    xhrSearch.onreadystatechange = function () {
+      if (xhrSearch.readyState == 4 && xhrSearch.status == 200) {
+        searchData = JSON.parse(xhrSearch.responseText);
+        if (keyword) {
+          input.value = decodeURI(keyword);
+          search(decodeURI(keyword));
+        }
+        input.placeholder = "请输入关键词，回车搜索";
+      }
+    }
+    xhrSearch.send(null);
+
+    document.querySelector('.search-input').addEventListener('keyup', function (e) {
+      if (e.keyCode == 13) {
+        search(decodeURI(this.value));
+      }
+    })
+
+    function search(keyword) {
       result.innerHTML = '';
       var title = '搜索：' + keyword + ' | ' + site.title;
       var url = '/search/?keyword=' + keyword;
@@ -33,3 +74,54 @@ function search(keyword) {
         _hmt.push(['_trackPageview', url]);
       }
     }
+
+  }
+
+
+  if (page.url == '/tags/') {
+    var keyword = getQuery('keyword');
+    var tagsData;
+    var xhrPosts = new XMLHttpRequest();
+    xhrPosts.open('GET', '/posts.json', true);
+    xhrPosts.onreadystatechange = function () {
+      if (xhrPosts.readyState == 4 && xhrPosts.status == 200) {
+        tagsData = JSON.parse(xhrPosts.responseText);
+        if (keyword) {
+          tags(decodeURI(keyword));
+        }
+      }
+    }
+    xhrPosts.send(null);
+    function tags(keyword) {
+      var title = '标签：' + keyword + ' | ' + site.title;
+      var url = '/tags.html?keyword=' + keyword;
+      var tagsTable = document.getElementById('tags-table');
+      tagsTable.style.display = 'table';
+      tagsTable.querySelector('thead tr').innerHTML = '<th colspan=2>以下是标签含有“' + keyword + '”的所有文章</th>';
+      var html = '';
+      tagsData.forEach(function (item) {
+        if (item.tags.indexOf(keyword) > -1) {
+          var date = item.date.slice(0, 10).split('-');
+          date = date[0] + ' 年 ' + date[1] + ' 月 ' + date[2] + ' 日';
+          html += '<tr><td><time>' + date + '</time></td><td><a href="' + item.url + '" title="' + item.title + '">' + item.title + '</a></td></tr>';
+        }
+      })
+      tagsTable.getElementsByTagName('tbody')[0].innerHTML = html;
+      document.title = title;
+      history.replaceState({
+        "title": title,
+        "url": url
+      }, title, url);
+      if (site.home === location.origin && window.parent == window) {
+        _hmt.push(['_trackPageview', url]);
+      }
+    }
+    var tagLinks = document.getElementsByClassName('post-tags-item');
+    var tagCount = tagLinks.length;
+    for (var i = 0; i < tagCount; i++) {
+      tagLinks[i].addEventListener('click', function (e) {
+        tags(e.currentTarget.title);
+        e.preventDefault();
+      }, false);
+    }
+  }
