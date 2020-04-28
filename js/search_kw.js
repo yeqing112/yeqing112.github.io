1924cc80c71409254f2a6875e46f034cd4d71b78
yeqing112 <51025826@qq.com>
2020-04-28 14:17:01 +0800

    Update search_kw.js

--- js/search_kw.js
+++ js/search_kw.js
@@ -4,21 +4,6 @@ window.addEventListener('beforeunload', function (event) {
 
 document.addEventListener('DOMContentLoaded', function (event) {
 
-  var disq = new iDisqus('comment', {
-    forum: site.forum,
-    site: site.home,
-    api: site.api + '/disqus',
-    title: page.title,
-    url: page.url,
-    mode: 2,
-    timeout: 3000,
-    slug: page.url.slice(1).split('.')[0],
-    init: true,
-    toggle: 'comment-toggle',
-    sort: 'newest',
-    emoji_path: site.api + '/emoji/unicode/',
-  });
-
   disq.count();
   timeAgo();
 
