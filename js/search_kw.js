31a087b80d330fd8cba2b8288945ed03f80acfcf
yeqing112 <51025826@qq.com>
2020-04-29 18:10:48 +0800

    Update search_kw.js

--- js/search_kw.js
+++ js/search_kw.js
@@ -323,17 +323,8 @@ document.addEventListener('DOMContentLoaded', function (event) {
       var first = (pageNum - 1) * pageSize;
       var last = total > pageNum * pageSize ? pageNum * pageSize : total;
 
-      if (page.url == '/ui/') {
-        for (var i = first; i < last; i++) {
-          var item = posts[i];
-          html += '<article class="post-item">' +
-            '    <section class="post-item-summary">' +
-            '    <h3 class="post-item-title"><a class="post-item-link" href="' + item.url + '" title="' + item.title + '">' + item.title + '</a></h3>' +
-            '    <time class="post-item-date timeago" datetime="' + item.date + '"></time>' +
-            '    </section>' +
-            '</article>';
-        }
-	  }
+
+          html += total;
 
       var totalPage = Math.ceil(total / pageSize);
       var prev = pageNum > 1 ? pageNum - 1 : 0;
