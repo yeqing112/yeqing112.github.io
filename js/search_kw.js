299f3b1a084e309500f4a299ce5e64e0cbcadda1
yeqing112 <51025826@qq.com>
2020-04-29 15:19:08 +0800

    Update search_kw.js

--- js/search_kw.js
+++ js/search_kw.js
@@ -324,14 +324,15 @@ document.addEventListener('DOMContentLoaded', function (event) {
       var last = total > pageNum * pageSize ? pageNum * pageSize : total;
 
       if (page.url == '/ui/') {
-
+        for (var i = first; i < last; i++) {
+          var item = posts[i];
           html += '<article class="post-item">' +
             '    <section class="post-item-summary">' +
-            '    <h3 class="post-item-title"><a class="post-item-link" href="" title="">111</a></h3>' +
-            '    <time class="post-item-date timeago" datetime="222"></time>' +
+            '    <h3 class="post-item-title"><a class="post-item-link" href="' + item.url + '" title="' + item.title + '">' + item.title + '</a></h3>' +
+            '    <time class="post-item-date timeago" datetime="' + item.date + '"></time>' +
             '    </section>' +
             '</article>';
-
+        }
 	  }
 
       var totalPage = Math.ceil(total / pageSize);
