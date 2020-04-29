e71bfe3a6e8425cd298fc1c1eb594e6994cc6805
yeqing112 <51025826@qq.com>
2020-04-29 13:52:34 +0800

    Update search_kw.js

--- js/search_kw.js
+++ js/search_kw.js
@@ -318,7 +318,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
       }
       var title = pageNum == 1 ? cat + ' | ' + site.title : cat + '：第' + pageNum + '页 | ' + site.title;
       var url = pageNum == 1 ? page.url : page.url + '?page=' + pageNum;
-      var html = pageNum;
+      var html = '';
       var total = posts.length;
       var first = (pageNum - 1) * pageSize;
       var last = total > pageNum * pageSize ? pageNum * pageSize : total;
@@ -326,12 +326,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
       if (page.url == '/ui/') {
         for (var i = first; i < last; i++) {
           var item = posts[i];
-          html += '<article class="post-item">' +
-            '    <section class="post-item-summary">' +
-            '    <h3 class="post-item-title"><a class="post-item-link" href="' + item.url + '" title="' + item.title + '">' + item.title + '</a></h3>' +
-            '    <time class="post-item-date timeago" datetime="' + item.date + '"></time>' +
-            '    </section>' +
-            '</article>';
+          html += '<article class="post-item">';
         }
 	  }
 
