dbab0476c6ccd76e6dad92cea8cab567a9663106
yeqing112 <51025826@qq.com>
2020-05-28 18:18:21 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -501,7 +501,7 @@ document.addEventListener('DOMContentLoaded', function (event) {
 	
     // 添加TOC关闭按钮
 		var toc = document.querySelector('.post-toc');
-		var tocbutton = '<div class = "close"></div>';
+		var tocbutton += '<div class = "close"></div>';
 		
 		if (toc) {
 			document.querySelector('.post-toc').innerHTML = tocbutton;
